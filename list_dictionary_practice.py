bbec7e513fd3d033fbc5ff2bb4c35fc4d3863e80
Zunair_Riaz <ranazunairriaz@gmail.com>
2025-07-02 01:04:15 +0500

    Add files via upload

---
+++ list_dictionary_practice.py
@@ -0,0 +1,28 @@
+fruits = ["Jamnu", "Limu", "Malta", "Kinnu", "Dawana"]
+print("Fruits names in Punjabi are : ", fruits)
+print("PANJWA FRUIT :", fruits[4])
+fruits.append("teenday le lo, kaddu le lo, kareleya")
+
+# print("tashee shuda phalon ki list :", fruits)
+# Explanation:*
+# – fruits is a list containing names of fruits.
+# – fruits[0] accesses the first item in the list.
+
+
+Name = {
+    "Zunair" : "Riaz",
+    "Sister" : 2,
+    "Love"  : "Coding & Cybersecurity",
+}
+
+# Display the entire dictionary
+print("Name is : ", Name)
+# Access a specific value by key
+print("Love is ", Name["Love"])
+# Add a new key-value pair
+Name["Profession"] = "FC"
+print("Update Personnel Info :", Name)
+
+# – person is a dictionary containing keys like “name”, “age”, and “city” with their respective values.
+# – person[“name”] retrieves the value associated with the “name” key, which is “Ali”.
+# – A new key-value pair, “profession”: “Engineer”, is added to the dictionary.
